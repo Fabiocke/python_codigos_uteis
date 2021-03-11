50a44b46614ef0d3f51f07244df91cb941821462
Fábio Teixeira <fabio.teixeira@xpi.com.br>
2021-03-11 12:10:48 -0300

    webfiles

---
+++ scrapping/webfiles.py
@@ -0,0 +1,13 @@
+
+# ler um arquivo txt ou csv direto da web retornando um pandas
+def read_txt_web(url, sep=';'):
+    import pandas as pd
+    import requests
+    r=requests.get(url).text
+    l=[i.strip().split(sep) for i in r.split('\n')]
+    df=pd.DataFrame(l[1:], columns=l[0])
+    return df
+
+
+
+
